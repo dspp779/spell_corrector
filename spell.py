e4ac0dd850fa3aaf01e3c9b3ff8897b6bd4b3b33
JJC <jjc@nlplab.cc>
2016-02-21 20:00:58 +0800

    spell corrector handling fusion errors

--- spell.py
+++ spell.py
@@ -1,16 +1,24 @@
-import re, collections
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from collections import defaultdict
+import re
 
-def words(text): return re.findall('[a-z]+', text.lower()) 
+def words(text): return re.findall('[a-z]+', text.lower())
+
+def words_and_bigrams(text):
+  all_words = words(text)
+  bigrams = [ ' '.join(all_words[i:i+2]) for i in range(len(all_words)-1) ]
+  return all_words + bigrams
 
 def train(features):
-    model = collections.defaultdict(lambda: 1)
+    model = defaultdict(lambda: 1)
     for f in features:
         model[f] += 1
     return model
 
-NWORDS = train(words(file('big.txt').read()))
+NWORDS = train(words_and_bigrams(file('big.txt').read()))
 
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
+alphabet = 'abcdefghijklmnopqrstuvwxyz\''
 
 def edits1(word):
    splits     = [(word[:i], word[i:]) for i in range(len(word) + 1)]
@@ -25,9 +33,20 @@ def known_edits2(word):
 
 def known(words): return set(w for w in words if w in NWORDS)
 
+# list all possible fusion error candidate
+def fusions_edit(word):
+   word        = word.replace(' ', '')
+   splits     = [(word[:i], word[i:]) for i in range(1, len(word))]
+   fusions    = [(a+' '+b) for a, b in splits if a in NWORDS and b in NWORDS]
+   if word in NWORDS:
+      fusions += [word]
+   return fusions
+
 def correct(word):
-    candidates = known([word]) or known(edits1(word)) or known_edits2(word) or [word]
+    candidates = known([word]) or fusions_edit(word) or known(edits1(word)) or known_edits2(word) or [word]
     return max(candidates, key=NWORDS.get)
 
-print 'speling ->', correct('speling')
-print 'korrecter ->', correct('korrecter')
+if __name__ == '__main__':
+    examples = ['taketo', 'mor efun', 'with out']
+    for example in examples:
+        print example, '->', correct(example)
