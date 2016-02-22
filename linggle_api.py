5053b0852e5e957e21bc76430b3c5adfe0a68018
JJC <jjc@nlplab.cc>
2016-02-22 16:48:12 +0800

    solve duplication result problem

--- linggle_api.py
+++ linggle_api.py
@@ -10,7 +10,13 @@ def linggleit(query):
     url = 'http://linggle.com/query/{}'.format(urllib.quote(query, safe=''))
     r = requests.get(url)
     if r.status_code == 200:
-        return map(parseData, r.json())
+        # return map(parseData, r.json())
+        # solve duplication problem
+        res = dict()
+        for ngram, count in map(parseData, r.json()):
+            if ngram not in res:
+                res[ngram] = count
+        return sorted(res.items(), key=lambda x: x[-1], reverse=True)
 
 if __name__ == "__main__":
     res = linggleit('adj./n. beach')
